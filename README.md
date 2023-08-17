edd3ed8a2b3632775d32e316cb51f5648b1c1fee
Redoge <mail.redoge@gmail.com>
2023-08-17 17:51:51 +0300

    changed files

--- README.md
+++
@@ -1,1 +0,0 @@
-# Redoge.github.io
---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Preload 
+window.onload = function() {
+    document.body.classList.add('loaded');
+}
+
+window.onload = function() {
+        document.body.classList.add('loaded_hiding');
+        window.setTimeout(function() {
+            document.body.classList.add('loaded');
+            document.body.classList.remove('loaded_hiding');
+        }, 500);
+    }
+    //Preload
